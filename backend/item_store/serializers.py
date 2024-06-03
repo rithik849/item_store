c49af8a4066705e7df4e655a61c013ebdd76a99b
Rithik Patel <rithiknpatel@gmail.comqq>
2024-06-03 16:46:29 +0100

    Add serializers for model classes

--- backend/item_store/serializers.py
+++ backend/item_store/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from item_store.models import Customer
+from item_store.models import Customer, Review, Basket, Order
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -21,4 +21,17 @@ class CustomerSerializer(serializers.ModelSerializer):
         fields = ('username', 'email','password')
         
 
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+    
+class BasketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Basket
+        fields = '__all__'
         
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
