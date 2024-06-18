0d927355e693fbd638cc337e6b366f3b7136933c
Rithik Patel <rithiknpatel@gmail.comqq>
2024-06-18 23:22:27 +0100

    Clean up urls

--- backend/e_store/urls.py
+++ backend/e_store/urls.py
@@ -15,38 +15,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import login,logout, authenticate
 from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import routers, serializers, viewsets
+from rest_framework.decorators import api_view, permission_classes
 from django.urls import path, include
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from django.views.decorators.csrf import csrf_protect, ensure_csrf_cookie, requires_csrf_token
 
-# class CustomerTokenPairView(TokenObtainPairView):
-#     permission_classes = (AllowAny ,)
-#     serializer_class = CustomerTokenPairSerializer
+from e_store.permissions import IsNotAuthenticated
     
-#     def get(self,request):
-#         return Response(data={"test":"here"})
-    
-# class CustomerTokenRefreshView(TokenRefreshView):
-#     permission_classes = (IsAuthenticated ,)
-#     authentication_classes = [JWTAuthentication]
-#     serializer_class = CustomerTokenRefreshSerializer
 
 # Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-# router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('customer/',include('token_auth.urls')),
+    path('customer/',include('user_auth.urls')),
     path('admin/', admin.site.urls),
     path('product/',include("products.urls")),
     path('',include("item_store.urls"))
 ]
 
-urlpatterns += [path('api-auth/',include('rest_framework.urls', namespace='rest_framework'))]
+urlpatterns += [path('api-auth/',include('rest_framework.urls', namespace='rest_framework'))
+                ]
