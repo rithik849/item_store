9875200577ec2c633e23868df2fb41bbd41508e7
Rithik Patel <rithiknpatel@gmail.comqq>
2024-06-18 23:19:56 +0100

    Add update product serializer

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -4,6 +4,21 @@ from products.models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('id','name','price','type','stock')
+        fields = ('name','price','type','stock')
         
+class UpdateProductSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Product
+        fields = "__all__"
         
+    # Negative quantity, we remove product, positive we add product
+    def validate(self,data):
+        if self.instance.stock + data['quantity'] < 0: # type: ignore
+            raise Exception("Not enough of product "+str(self.instance)+" in stock")
+        return data
+    
+    def update(self,instance,validated_data):
+        instance.stock += validated_data['quantity']
+        instance.save()
+        return instance
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,22 +1,12 @@
-from django.shortcuts import get_object_or_404, render
-from rest_framework.fields import api_settings
-from rest_framework.response import Response
-from .serializers import ProductSerializer
-from products.models import Product
 from rest_framework import viewsets
-from rest_framework.pagination import PageNumberPagination
+from products.serializers import ProductSerializer
+from products.models import Product
 from rest_framework.mixins import RetrieveModelMixin, ListModelMixin
-
-class GetUserMixin:
-    
-    def get_object(self):
-        return self.request.user # type: ignore
 
 # Create your views here.
 class ProductViewSet(RetrieveModelMixin,ListModelMixin,viewsets.GenericViewSet): # type: ignore
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = []
-    pagination_class = PageNumberPagination
     
     
