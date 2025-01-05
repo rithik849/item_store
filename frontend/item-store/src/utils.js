d0fb580aadef7e4dbe3992e0f8b689831d2c4458
Rithik Patel <rithiknpatel@gmail.com>
2025-01-05 23:34:03 +0000

    Add function to generate request header

--- frontend/item-store/src/utils.js
+++ frontend/item-store/src/utils.js
@@ -1,4 +1,19 @@
+import {Cookies, useCookies } from "react-cookie"
 
 export const formatter = new Intl.NumberFormat('en-GB', {
     style: 'currency',
     currency: 'GBP',})
+
+export function getHeaders(){
+    const cookie = new Cookies()
+    const header = {
+        "Content-Type" : 'application/json; charset=UTF-8',
+        "Access-Control-Allow-Credentials" : true,
+    }
+
+    const csrftoken = cookie.get('csrftoken')
+    if (csrftoken){
+        header['X-CSRFToken'] = csrftoken
+    }
+    return header
+}
