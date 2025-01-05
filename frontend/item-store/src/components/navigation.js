efef61750e09cbff10b29151c61d983057e06d1f
Rithik Patel <rithiknpatel@gmail.com>
2025-01-05 23:41:03 +0000

    Ensure displayed user details are up to date

--- frontend/item-store/src/components/navigation.js
+++ frontend/item-store/src/components/navigation.js
@@ -1,31 +1,42 @@
 import { NavLink } from "react-router-dom";
 import { useAuth } from "./is_authenticated_component";
 import { Outlet } from "react-router-dom";
-
+import { useEffect, useState } from "react";
 
 export function Navigation(props){
 
     const {isAuthenticated,user,logout,login} = useAuth()
+    const [username,setUsername] = useState("")
+
+    useEffect(()=>{
+        if (user!==""){
+            setUsername(user.username)
+        }else{
+            setUsername("")
+        }
+    },[user.username,user.email])
 
     return (
         <div>
-            <nav>
-                <NavLink to={""}>{'Home'}</NavLink>
-                {isAuthenticated && 
-                <>
-                <NavLink to='/profile'>Profile</NavLink>
-                <NavLink to='/orders'>Orders</NavLink>
-                <NavLink to='/baskets'>Basket</NavLink>
-                <NavLink to='/logout'>Log Out</NavLink>
-                </>
-                }
-                {!isAuthenticated &&
-                <>
-                <NavLink to='/login'>Log In</NavLink>
-                </>
-                }
-            </nav>
-            {isAuthenticated && <p>{user.username}</p>}
+            <div className='d-flex justify-content-center py-3'>
+                <nav className="nav nav-pills">
+                    <NavLink className='nav-item navbar-item' to={""}>{'Home'}</NavLink>
+                    {isAuthenticated && 
+                    <>
+                    <NavLink className='nav-item navbar-item' to='/profile'>Profile</NavLink>
+                    <NavLink className='nav-item navbar-item' to='/orders'>Orders</NavLink>
+                    <NavLink className='nav-item navbar-item' to='/baskets'>Basket</NavLink>
+                    <NavLink className='nav-item navbar-item' to='/logout'>Log Out</NavLink>
+                    </>
+                    }
+                    {!isAuthenticated &&
+                    <>
+                    <NavLink className='nav-item navbar-item' to='/login'>Log In</NavLink>
+                    </>
+                    }
+                </nav>
+            </div>
+            {isAuthenticated && <p>{username}</p>}
             {<Outlet/>}
         </div>
 
