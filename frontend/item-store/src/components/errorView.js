d3ec224a68c99615ff6800908bfd7d8d3ea9f468
Rithik Patel <rithiknpatel@gmail.com>
2025-01-16 23:59:45 +0000

    Make error view redirect to main page

--- frontend/item-store/src/components/errorView.js
+++ frontend/item-store/src/components/errorView.js
@@ -1,9 +1,20 @@
+import { useEffect } from "react"
+import {useNavigate} from "react-router-dom"
 
 
 
 export function ErrorView({message}){
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        setTimeout(() => {
+            navigate('/')
+        }, 2000)
+    },[navigate])
+
     return (
-        <div>
+        <div className='error'>
             <h1>
                 {message}
             </h1>
