44648e72f30ba7d14bd59cb7fcfc308c7ba70976
Rithik Patel <rithiknpatel@gmail.com>
2025-01-18 19:48:15 +0000

    Remove display of id for products

--- frontend/item-store/src/Product/product_card.js
+++ frontend/item-store/src/Product/product_card.js
@@ -5,7 +5,6 @@ function Product({values}){
 
     return (
     <div>
-        <h2>{values.id}</h2>
         <h2>{"Name: "+values.name} </h2>
         <h2>{"Price: "+formatter.format(values.price)}</h2>
         <h3>{"Stock: "+values.stock}</h3>
